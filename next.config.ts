0238dab55d4f772e6ceefdc8fd5de73d88cd7e34
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2024-11-27 13:04:55 +0700

    UploadThing Setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "uploadthing.com",   // Thêm domain "uploadthing.com"
+      "utfs.io"            // Thêm domain "utfs.io" để hỗ trợ tải ảnh từ đây
+    ]
+  }
 };
 
 export default nextConfig;
